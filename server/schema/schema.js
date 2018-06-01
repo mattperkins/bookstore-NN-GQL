17425f27cf9e85cd36635f6d9ec4636347477e67
mattperkins <github@mattperkins.com>
2018-06-01 17:46:16 +0100

    add dummy data

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -2,6 +2,13 @@ const graphql = require('graphql')
 
 const {GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql
 
+// dummy data
+var books = [
+    {name: 'Lord of the rings', genre:'fantasy', id:1},
+    {name: '1984', genre:'drama', id:2},
+    {name: 'Lord of the flies', genre:'thriller', id:3}
+]
+
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
@@ -24,6 +31,12 @@ const RootQuery = new GraphQLObjectType({
         }
     }
 })
+
+// book(id: "2"){
+//     name
+//     genre
+// }
+
 
 module.exports = new GraphQLSchema({
     query: RootQuery
