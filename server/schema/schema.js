cb86fdec2db3c0dff8fb8a380057b63d4566d757
mattperkins <github@mattperkins.com>
2018-06-02 00:30:53 +0100

    cl

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,4 +1,5 @@
 const graphql = require('graphql')
+const _ = require('lodash')
 
 const {GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql
 
@@ -25,13 +26,17 @@ const RootQuery = new GraphQLObjectType({
             type: BookType,
             args:{id:{type: GraphQLString}},
             resolve(parent, args){
-                args.id
-                // code to get data db 
+                // code to get data from db or local dummy data
+                // lodash
+                return _.find(books, {
+                    id: args.id
+                })
             }
         }
     }
 })
 
+// example query shape
 // book(id: "2"){
 //     name
 //     genre
